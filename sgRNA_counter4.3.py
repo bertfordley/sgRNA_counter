26e3384dc6b4076354d26ebe3119a3fabad56e19
Rob Moseley <rmosele4@vols.utk.edu>
2018-11-05 10:21:48 -0500

    changed from parallel to single

--- sgRNA_counter4.3.py
+++ sgRNA_counter4.3.py
@@ -1,101 +1,121 @@
 # -*- coding: utf-8 -*-
 """
 By: Rob Moseley
-Spyder Editor
 
 Script for counting sgRNA in fastq files.
 Uses SequenceMatcher of the difflib package to find matches between the 
 sgRNA (and other parts) sequences and reads in the fastq file.
 """
 
-import pandas as pd
+import pymongo
+import pprint
+import json
 import os
 import gzip
 import itertools
-from joblib import Parallel, delayed
+# !pip install --user fuzzywuzzy
+# !pip install --user python-Levenshtein
 import fuzzywuzzy.process
+import pandas as pd
+import argparse
+
+def query_db_for_fastq_file(fastq_file_name):
+    
+    dbURI = 'mongodb://readonly:WNCPXXd8ccpjs73zxyBV@data-catalog.sd2e.org:27020/admin?readPreference=primary'
+    client = pymongo.MongoClient(dbURI)
+    db = client.catalog_dev
+    science_table=db.science_table
+    query={}
+    query["experiment_reference"] = "YeastSTATES-gRNA-Seq-Diagnosis"
+    query['lab']='Ginkgo'
+    query['measurement_type'] = 'RNA_SEQ'
+    query['file_type'] = 'FASTQ'
+    all_sgrna = []
+    for sample in science_table.find(query):
+        all_sgrna.append(sample)
+    
+    fastq_metadata = {}
+    for sample in all_sgrna:
+        try:
+            if sample['strain_input_state'] is not None and os.path.basename(sample["filename"]) == fastq_file_name:
+                fastq_metadata = sample
+        except Exception as e: continue   
+
+    return fastq_metadata
 
 
-def reverse_complement(seq): 
+def get_metadata(sample):
+    # Get metadata for sample
     
-    alt_map = {'ins': '0'}
-    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'} 
+    out_dict = {}
     
-    for k, v in alt_map.items():
-        seq = seq.replace(k, v)
+    out_dict["gate"] = sample["strain_circuit"]
+    out_dict["left_signal"] = sample["strain_input_state"][0]
+    out_dict["right_signal"] = sample["strain_input_state"][1]
+    out_dict["R1_fastq_filename"] = os.path.basename(sample["filename"])
+    out_dict["sample_id"] = sample["sample_id"]
+    out_dict["reference_sample_id"] = sample["reference_sample_id"]
+    out_dict["replicate"] = sample["replicate"]
+    out_dict["strain_lab_id"] = sample["strain_lab_id"]
+    out_dict["strain"] = sample["strain"]
         
-    bases = list(seq) 
-    bases = reversed([complement.get(base, base) for base in bases])
-    bases = ''.join(bases)
+    return out_dict
+
+
+def create_logic_count_dict(meta_info_dict, part_seq_dict, circuit_dict):
+    # extend metadata dictionary to include circuit information, e.g., input signal state,
+    # which sgRNA are the inputs and which are the intermediates. Also add in key:value
+    # pairs for when counting occurs
     
-    for k, v in alt_map.items():
-        bases = bases.replace(v, k)
-        
-    return bases
-
-
-def getGatePartSeqs(gate_seqs_file):
-
-    one_gate_seqs_dict = {}
+    gate_name = meta_info_dict["gate"]
     
-    with open(gate_seqs_file) as f:
-        seqs = [line.rstrip('\n') for line in f]
-        for s in seqs:
-            name, seq = s.split(":")
-            one_gate_seqs_dict[name] = seq
-        
-    return one_gate_seqs_dict
-
-
-def read_fastq_seqs(filepath):
-
-    with gzip.open(filepath, 'rt', encoding='utf') as fh:
-        for seq_header, seq, qual_header, qual in itertools.zip_longest(*[fh] * 4):
-#             if any(line is None for line in (seq_header, seq, qual_header, qual)):
-#                 raise Exception(
-#                     "Number of lines in FASTQ file must be multiple of four "
-#                     "(i.e., each record must be exactly four lines long).")
-#             if not seq_header.startswith('@'):
-#                 raise Exception("Invalid FASTQ sequence header: %r" % seq_header)
-#             if qual_header != '+\n':
-#                 raise Exception("Invalid FASTQ quality header: %r" % qual_header)
-#             if qual == '\n':
-#                 raise Exception("FASTQ record is missing quality scores.")
-
-            yield seq.rstrip('\n')
-
-
-def createLogicCountDict(filename, gate_name, seq_dict, circuit_dict):
-
-    logic_count_dict = {}
-    logic_count_dict["fastq_R1_filename"] = filename
-    logic_count_dict["left_input"] = circuit_dict[gate_name]["input"][0]
-    logic_count_dict["right_input"] = circuit_dict[gate_name]["input"][1]
+    meta_info_dict["left_input"] = circuit_dict[gate_name]["input"][0]
+    meta_info_dict["right_input"] = circuit_dict[gate_name]["input"][1]
 
     for i, part in enumerate(circuit_dict[gate_name]["intermediate"]):
-        logic_count_dict["intermediate" + str(i + 1)] = part
+        if part:
+            meta_info_dict["intermediate" + str(i + 1)] = part
+        else:
+            meta_info_dict["intermediate" + str(i + 1)] = "-" 
 
-    for part, seq in seq_dict.items():
-        logic_count_dict[part] = 0
+    for part, seq in part_seq_dict.items():
+        meta_info_dict[part] = 0
 
-    return logic_count_dict
+    return meta_info_dict
 
 
-def returnGategRNA(gatename, seq_dict, gate_dict):
+def return_gate_grna(meta_info_dict, part_seq_dict, circuit_dict):
+    # make a list of appropriate sgRNA sequences for sample being analyzed to be
+    # used for matching to reads in fastq file. Also make dictionary specifically for
+    # the gate that includes sgRNA names and sequences for matching.
+    
+    gate_name = meta_info_dict["gate"]
     
     gRNA_list = []
     gate_part_dict = {}
     
-    for part, seq in seq_dict.items():
-        if part in gate_dict[gatename]["input"] or part in gate_dict[gatename]["intermediate"]:
+    for part, seq in part_seq_dict.items():
+        if part in circuit_dict[gate_name]["input"] or part in circuit_dict[gate_name]["intermediate"]:
             gRNA_list.append(seq)
             gate_part_dict[part] = seq
     
     return gRNA_list, gate_part_dict
 
 
-def matchParts(line_1, line_2, gRNA_seqs, gate_part_dict, thres):
+def read_fastq_seqs(filepath):
+    # unzip and read fastq file
+    
+    with gzip.open(filepath, 'rt', encoding='utf') as fh:
+        for seq_header, seq, qual_header, qual in itertools.zip_longest(*[fh] * 4):
+            
+            yield seq.rstrip('\n')
 
+
+def match_parts(line_1, line_2, gRNA_seqs, gate_part_dict, thres):
+    # match sgRNAs in the cicuit to reads in the fastq R1 and R2 files
+    # fuzzy matching is used with user picking a threshold for percent
+    # match. Default is 80.
+    
     line_1_rev = reverse_complement(line_1)
     line_2_rev = reverse_complement(line_2)
 
@@ -128,120 +148,130 @@ def matchParts(line_1, line_2, gRNA_seqs, gate_part_dict, thres):
     return parts_list
 
 
-def countFile(onefile, wdir, meta_df, gate_seqs_dict, gate_dict, idx, num_batches, batch_num, num_files, thres):
+def reverse_complement(seq): 
+    # make reverese complement of read
     
-    if onefile == None: 
-        return {}
-    else:
-        filename_R1 = os.path.join(wdir, onefile)
-        filename_R2 = filename_R1[:-10] + "2.fastq.gz"
-        print("File {} of {} in Batch {} of {}: {}, {}".format(idx + 1, num_files, batch_num + 1, num_batches, os.path.basename(filename_R1), os.path.basename(filename_R2)))
-        
-        gate = meta_df.loc[meta_df["fastq_R1_filename"] == onefile]["gate"].values[0]
+    alt_map = {'ins': '0'}
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'} 
     
-        logic_patternCounts = createLogicCountDict(onefile, gate, gate_seqs_dict, gate_dict)
+    for k, v in alt_map.items():
+        seq = seq.replace(k, v)
         
-        gRNA_seq_list, gate_part_dict = returnGategRNA(gate, gate_seqs_dict, gate_dict)
-        
-        seq_gen_r1 = read_fastq_seqs(filename_R1)
-        seq_gen_r2 = read_fastq_seqs(filename_R2)
-        
-        for (line_r1, line_r2) in zip(seq_gen_r1, seq_gen_r2):
-            parts_list = matchParts(line_r1, line_r2, gRNA_seq_list, gate_part_dict, thres)
-            if parts_list:
-                for part in parts_list:
-                    logic_patternCounts[part] += 1
+    bases = list(seq) 
+    bases = reversed([complement.get(base, base) for base in bases])
+    bases = ''.join(bases)
     
-        #return pd.DataFrame(logic_patternCounts, index=[1])
-        return(logic_patternCounts)
-
-def grouper(n, iterable, fillvalue=None):
-    "grouper(3, ('cat','dog','fish','human'), 'x') --> ('cat','dog','fish'), ('human',None,None)"
-    args = [iter(iterable)] * n
-    return itertools.zip_longest(fillvalue=fillvalue, *args)
+    for k, v in alt_map.items():
+        bases = bases.replace(v, k)
+        
+    return bases
 
 
-def getMeta(meta_file_name, wdir, media):
+def count_file(sample, gate_seqs_dict, gate_info_dict, threshold):
+    # process a single fastq file for counting sgRNA
     
-    meta_file = pd.read_csv(os.path.join(wdir, meta_file_name), index_col=0)
-    meta_file_sliced = meta_file.loc[(meta_file["Media"] == media) & 
-                               (meta_file["fastq_R1_filename"].str.contains("gz")) & 
-                               (~meta_file["gate"].str.contains("wt", na=False))]
-        
-    return meta_file_sliced
+    # get metadata dictionary
+    meta_info_dict = get_metadata(sample)
+    
+    # get names for both R1 and R2 files for sample
+    filename_R1 = sample["jupyter_path"]
+#     filename_R1 = os.path.join(fastq_dir, os.path.basename(meta_info_dict["R1_fastq_filename"]))
+    filename_R2 = filename_R1[:-10] + "2.fastq.gz"
 
+    print("Gate {}, Input State {}{}: {} {}".format(meta_info_dict["gate"],
+                                                    meta_info_dict["left_signal"],
+                                                    meta_info_dict["right_signal"],
+                                                    os.path.basename(filename_R1),
+                                                    os.path.basename(filename_R2)))
     
+    # expand metadata dictionary to include circuit info
+    logic_patternCounts = create_logic_count_dict(meta_info_dict, gate_seqs_dict, gate_info_dict)
+
+    # get specific gate sgRNA name and sequences
+    gRNA_seq_list, gate_part_dict = return_gateg_rna(meta_info_dict, gate_seqs_dict, gate_info_dict)
+
+    # read R1 and R2 fastq files and make iterators for both
+    seq_gen_r1 = read_fastq_seqs(filename_R1)
+    seq_gen_r2 = read_fastq_seqs(filename_R2)
+
+    # for each line pair in R1 and R2 fastq files, match sgRNAs found in sample's gate
+    for i, (line_r1, line_r2) in enumerate(zip(seq_gen_r1, seq_gen_r2)):
+        parts_list = match_parts(line_r1, line_r2, gRNA_seq_list, gate_part_dict, threshold)
+        if parts_list:
+            for part in parts_list:
+                print("Match in line {}: {}".format(i+1, part))
+                logic_patternCounts[part] += 1
+
+#     #return pd.DataFrame(logic_patternCounts, index=[1])
+    return(logic_patternCounts)
+
 if __name__ == "__main__":
-    # Set input variables
     
-    # Where meta file and gate seqs file are stored
-    maindir = "/Users/haase-admin/Desktop/rmoseley/sgRNA_counter"
+    parser = argparse.ArgumentParser(prog="sgRNA counter",
+                                     description="Using fuzzy string matching to find sgRNA target sequences within a fastq file")
+                            
+    parser.add_argument("fastq_R1_file", 
+                        action="store", 
+                        help="R1 fastq file containing reads, don't include path")
+                                  
+    parser.add_argument("-o", "--output_dir",
+                        type=str,
+                        default=None,
+                        required=True,
+                        help="output directory to write results")                                  
+                                     
+    parser.add_argument("-t", "--threshold",
+                        type = int,
+                        default=80,
+                        required=True,
+                        help="threshold for string matching")  
+                                     
+    args = parser.parse_args()
     
-    # Where fastq files are stored
-    filedir = "/Users/haase-admin/Desktop/rmoseley/sgRNA_counter/fastq_files"
-    output_file_name = "merged_meta_counts_data"
+    fastq_file = args.fastq_R1_file
+    threshold = args.threshold
+    output_dir = args.output_dir
     
-    # test_file_names = ["9918080-R1.fastq.gz", "9918096-R1.fastq.gz"]
-    # test_file_names = ["10004578-R1.fastq.gz", "10004562-R1.fastq.gz"]
-     
-    # testdir = "/Users/haase-admin/Desktop/rmoseley/test"
-    
-    media_selection = "standard"
-    meta_file_name = "full_meta_merge.csv"
-    gate_seq_file = "gate_seqs.txt"
-    
-    # Grab gate part sequences and meta data based on media of interest. 
-    # Then make list of fastq files from meta data for sgRNA counting
-    
-    # gate part sequences (dictionary)
-    gate_seqs_dict = getGatePartSeqs(os.path.join(maindir, gate_seq_file))
-    
-    # meta data file for 'standard' media (dataframe)
-    meta_file_stad = getMeta(meta_file_name, maindir, media_selection)
-    
-    # list of fastq file names (list)
-    full_file_list = meta_file_stad["fastq_R1_filename"].tolist()
-    # full_file_list = ["10004550-R1.fastq.gz", "9918101-R1.fastq.gz", "9249509-R1.fastq.gz", "10003155-R1.fastq.gz"]
+    gate_dict = {"OR": {"input": ["r3", "r6"], "intermediate": ["r7", "", ""], 
+                    "additional": ["HHRz5p", "HHRz3p", "sgRNA_handle", "HDVRz"]},
+             "AND": {"input": ["r2", "r1"], "intermediate": ["r7", "r5", ""], 
+                     "additional": ["HHRz5p", "HHRz3p", "sgRNA_handle", "HDVRz"]},
+             "NAND": {"input": ["r10", "r2"], "intermediate": ["r1", "r3", "r5"], 
+                      "additional": ["HHRz5p", "HHRz3p", "sgRNA_handle", "HDVRz"]},
+             "XNOR": {"input": ["r2", "r10"], "intermediate": ["r7", "r3", "r1"], 
+                      "additional": ["HHRz5p", "HHRz3p", "sgRNA_handle", "HDVRz"]},
+             "XOR": {"input": ["r3", "r6"], "intermediate": ["r1", "r9", "r7"], 
+                     "additional": ["HHRz5p", "HHRz3p", "sgRNA_handle", "HDVRz"]}}
 
-    # Form circuits. First two indices in values are the inputs for the respective gate.
-    # First index is left-side of truth table and second index is right-side of truth table
-    # from Gander et al. 2017. For example, "OR" gate's input is "r3" and "r6".
-    # For 0,1 in the truth table, "r3" is turned off and "r6" is turned on.
-    
-    #                   "input": [left, right]
-    gate_dict = {"OR": {"input": ["r3", "r6"], 
-                               "intermediate": ["r7", "", ""], 
-                               "additional": ["HHRz5p", "HHRz3p", "sgRNA_handle", "HDVRz"]},
-                       "AND": {"input": ["r2", "r1"], 
-                               "intermediate": ["r7", "r5", ""], 
-                               "additional": ["HHRz5p", "HHRz3p", "sgRNA_handle", "HDVRz"]},
-                       "NAND": {"input": ["r10", "r2"], 
-                                "intermediate": ["r1", "r3", "r5"], 
-                                "additional": ["HHRz5p", "HHRz3p", "sgRNA_handle", "HDVRz"]},
-                       "XNOR": {"input": ["r2", "r10"], 
-                                "intermediate": ["r7", "r3", "r1"], 
-                                "additional": ["HHRz5p", "HHRz3p", "sgRNA_handle", "HDVRz"]},
-                       "XOR": {"input": ["r3", "r6"], 
-                               "intermediate": ["r1", "r9", "r7"], 
-                               "additional": ["HHRz5p", "HHRz3p", "sgRNA_handle", "HDVRz"]}}
+    gate_part_seqs = {'r1': 'GGAACGTGATTGAATAACTT',
+                      'r2': 'ACCAACGCAAAAAGATTTAG',
+                      'r3': 'CATTGCCATACACCTTGAGG',
+                      'r4': 'GAAAATCACAACTCTACTGA',
+                      'r5': 'GAAGTCAGTTGACAGAGTCG',
+                      'r6': 'GTGGTAACTTGCTCCATGTC',
+                      'r7': 'CTTTACGTATAGGTTTAGAG',
+                      'r8': 'CGCATTTCCTATTCAAACTT',
+                      'r9': 'GCAACCCACAAATATCCAGT',
+                      'r10': 'GTGACATAAACATTCGACTC',
+                      'r11': 'GGGCAAAGAGACGCTTGTCG',
+                      'r12': 'GAAGTCATCGCTTCTTGTCG',
+                      'r13': 'GAGTTGACAAAGTATAACTT',
+                      'r14': 'GAAGTTTCAGAATCTCGACG',
+                      'r15': 'GGCTAGGATCCATCTGACTT',
+                      'r16': 'GCAACCATAGACTCTCCAGG',
+                      'r17': 'ACCACAACTGAGTCGAACCT',
+                      'r18': 'GGGTAGCAACACTCGTACTT',
+                      'r19': 'GTAAAAGATAACTCTGTTGC',
+                      'r20': 'TCTACCCGAGACTCAAACGG',
+                      'HHRz5p': 'GGATTCTAGAACTAGTGGATCTACAAA',
+                      'HHRz3p': 'CTGATGAGTCCGTGAGGACGAAACGAGTAAGCTCGTC',
+                      'sgRNA_handle': 'GTTTTAGAGCTAGAAATAGCAAGTTAAAATAAGGCTAGTCCGTTATCAACTTGAAAAAGTGG',
+                      'HDVRz': 'CACCGAGTCGGTGCTTTTGGCCGGCATGGTCCCAGCCTCCTCGCTGGCGCCGGCTGGGCAACATGCTTCGGCATGGCGAATGGGACTGATACCGTCGACCTCGAGTC'}
 
-
-    # Parallel
-    # Count parts of each gate in respective fastq file
-    counts_dict_list = []
-    n_jobs = 10
-    total_batches = int(round(len(full_file_list)/n_jobs, 0) + 1)
-    sub_lists = grouper(n_jobs, full_file_list)
-    for batchi, sub_file_list in enumerate(sub_lists):
-        total_counts_df_list = Parallel(n_jobs=n_jobs)(delayed(countFile)(onefile, filedir, meta_file_stad, gate_seqs_dict, gate_dict, i, total_batches, batchi, len(sub_file_list), 80) for i, onefile in enumerate(sub_file_list))
-        total_counts_df_list = [x for x in total_counts_df_list if x != {}]
-        
-        counts_dict_list.append(pd.DataFrame(total_counts_df_list))
-
-        merged_meta_counts_df = pd.merge(meta_file_stad, pd.concat(counts_dict_list), how="outer", 
-                                         left_on="fastq_R1_filename", right_on="fastq_R1_filename")
-        
-        merged_meta_counts_df.to_csv(os.path.join(maindir, output_file_name + ".csv"))
-    
-    print("FINSIHED!!")
-    
+    fastq_metadata = query_db_for_fastq_file(fastq_file)
+            
+    if fastq_metadata is None:
+        print("Either {} isn't in database or no circuit information is available".format(fastq_file_name))
+    else:   
+        results = count_file(fastq_metadata, gate_part_seqs, gate_dict, threshold)
+        print(pd.DataFrame(results, index=[1]).T)                           
